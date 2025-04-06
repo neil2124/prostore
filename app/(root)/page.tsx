0b3bc090d478a7290a16abb2bf92c129967c6b75
Neil <nmascar@gmu.edu>
2025-04-06 18:10:53 -0400

    Added all files and working on the header and footer components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+  <div className="flex-between">
+    <Button>Button</Button>
+  </div>
+);
+};
+
+export default HomePage;
